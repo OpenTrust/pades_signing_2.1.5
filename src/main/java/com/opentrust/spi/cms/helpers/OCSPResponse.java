c6e3b45d2b89b9613b03bcd206f312846fb5a805
Phouric Ung <phouric.ung@opentrust.com>
2013-12-13 18:07:16 +0000

    fix ocsp response parameter conversion for SPI
    git-svn-id: svn+ssh://raki.keynectis.net/svnroot/sequoia/modules/SECURITY/trunk@41125 666fc95a-753c-42e9-8efc-9f4ba2ed62c0

--- src/main/java/com/opentrust/spi/cms/helpers/OCSPResponse.java
+++ src/main/java/com/opentrust/spi/cms/helpers/OCSPResponse.java
@@ -6,6 +6,9 @@ import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.ocsp.BasicOCSPResponse;
 import org.bouncycastle.ocsp.BasicOCSPResp;
+import org.bouncycastle.ocsp.OCSPException;
+import org.bouncycastle.ocsp.OCSPResp;
+import org.bouncycastle.ocsp.OCSPRespGenerator;
 
 
 public class OCSPResponse {
@@ -17,17 +20,32 @@ public class OCSPResponse {
 	public OCSPResponse(BasicOCSPResp ocsp) throws IOException
 	{
 		this.response = ocsp;
-		this.encoded = ocsp.getEncoded();
+		// We need the OCSP envelop for inclusion in CMS, so recreate the envelop
+		// from the OCSP basic response sequence
+		OCSPRespGenerator gen = new OCSPRespGenerator();
+		OCSPResp ocspResponse;
+        try {
+            ocspResponse = gen.generate(OCSPRespGenerator.SUCCESSFUL, ocsp);
+        } catch (OCSPException e) {
+            // should not happen
+            throw new IOException(e.toString(), e);
+        }
+		this.encoded = ocspResponse.getEncoded();
 	}
 	
 	public OCSPResponse(byte [] encoded) throws IOException
 	{
 		this.encoded = encoded;
-		ASN1InputStream ais = new ASN1InputStream(encoded);
-		ASN1Sequence readObject = (ASN1Sequence) ais.readObject();
+		ASN1Sequence readObject = parseAsn1(encoded);
 		
 		response = new BasicOCSPResp(new BasicOCSPResponse(readObject));
 	}
+
+    private ASN1Sequence parseAsn1(byte[] encoded) throws IOException {
+        ASN1InputStream ais = new ASN1InputStream(encoded);
+		ASN1Sequence readObject = (ASN1Sequence) ais.readObject();
+        return readObject;
+    }
 	
 	public byte[] getEncoded() {
 		return encoded;
